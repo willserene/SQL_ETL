2b7c9afa46692b82eecb002c7a14fdd06cb2e60a
Travbz <78450169+Travbz@users.noreply.github.com>
2021-05-26 19:55:37 -0600

    travbz schema

--- queries/create_schema.sql
+++ queries/create_schema.sql
@@ -1,6 +1,19 @@
+DROP TABLE IF EXISTS game_scratches;
 DROP TABLE IF EXISTS game_plays;
 DROP TABLE IF EXISTS game_teams_stats;
 DROP TABLE IF EXISTS game;
+DROP TABLE IF EXISTS team_info;
+
+CREATE TABLE team_info (
+	team_id int,
+	franchiseld int,
+	shortName varchar(80),
+	teamName varchar(80),
+	abbreviation varchar(3),
+	link varchar(30),
+	PRIMARY KEY (team_id)
+);
+
 
 
 CREATE TABLE game (
@@ -21,6 +34,7 @@ CREATE TABLE game (
 	venue_time_zone_tz varchar(3),
 	PRIMARY KEY (game_id)
 );
+
 
 
 CREATE TABLE game_plays (
@@ -65,4 +79,10 @@ CREATE TABLE game_teams_stats (
 	blocked int,
 	starRinkSide varchar(5),
 	FOREIGN KEY (game_id) REFERENCES game(game_id)
+);
+
+CREATE TABLE game_scratches (
+	game_id int,
+	team_id int,
+	player_id int
 );
