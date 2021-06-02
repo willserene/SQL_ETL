e8b104da421640a6beb589ab9cbd0c499e442e82
gldn-god <78330405+gldn-god@users.noreply.github.com>
2021-06-01 20:00:02 -0600

    removed old files, reran notebook for team, consolidated queries to analysis sql file

--- queries/analysis.sql
+++ queries/analysis.sql
@@ -1,11 +1,36 @@
-SELECT pi.* 
-FROM player_info pi
+/*
+Top 10 Goalies by Save Rate, having played at least 250 Games
+*/
+SELECT
+  pi."firstName" || ' ' || pi."lastName" as "Goalie Name",
+  pi.nationality as "Nationality",
+  count(distinct ggs.game_id) as "Games Played",
+  round((avg(ggs.saves)/avg(shots))*100, 2) as "Save Percentage"
+FROM game_goalie_stats ggs
+LEFT JOIN player_info pi ON pi.player_id = ggs.player_id
+GROUP BY pi."firstName" || ' ' || pi."lastName", pi.nationality
+HAVING count(distinct game_id) > 250
+ORDER BY round((avg(ggs.saves)/avg(shots))*100, 2) DESC
+LIMIT 10
 ;
 
-SELECT gss.player_id, sum(gss.goals)
-FROM game_skater_stats gss
-GROUP BY gss.player_id
+
+/*
+Wins by Coach (for each team coached)
+*/
+SELECT 
+	gts.head_coach as "Head Coach",
+	ti."shortName" || ' ' || ti."teamName" as "Team",
+	count(gts.won) filter (where gts.won) as "Games Won",
+	count(gts.won) as "Total Games"
+FROM game_teams_stats gts
+JOIN team_info ti on ti.team_id = gts.team_id
+WHERE gts.head_coach is not null
+GROUP BY gts.head_coach, ti."shortName" || ' ' || ti."teamName"
+ORDER BY gts.head_coach
 ;
+
+
 
 /*
 Top 10 Nations for Total Goals Made by Players (all history)
